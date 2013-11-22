6213e18919cb6fb6fd826a2fae6a5e185cdb8f2d
Project3 <proj3@localhost.localdomain>
2013-11-22 01:43:37 -0500

    fix bug

--- src/http_parser.c
+++ src/http_parser.c
@@ -164,14 +164,14 @@ void TputCalculation(SockData* proxy, double alpha)
   time(&(proxy->bitratedata.timer_f));
   gettimeofday(&(proxy->bitratedata.stop), NULL);
   printf("End time %s",asctime( localtime(&(proxy->bitratedata.timer_f))) );
-  printf("%ld\n", (long int)proxy->bitratedata.start.tv_usec);
+  printf("%ld\n", (long int)proxy->bitratedata.stop.tv_usec);
   
   duration = difftime(proxy->bitratedata.timer_f, proxy->bitratedata.timer_s);
   proxy->bitratedata.duration = duration;
   
   proxy->bitratedata.tput_new = (double)(proxy->bitratedata.chunksize*8) / (duration*1000);
-  if(proxy->bitratedata.tput_new > 1500)
-   proxy->bitratedata.tput_new = 1500;
+  if(proxy->bitratedata.tput_new > 2000)
+   proxy->bitratedata.tput_new = 2000;
   proxy->bitratedata.tput_current = (alpha) * (proxy->bitratedata.tput_new) + (1-alpha) * (proxy->bitratedata.tput_current);
   printf("tput_current is %f\n", proxy->bitratedata.tput_current);
   //logging
